72853e0ffb67bea7704fea8868f9624f8493a3bd
Bill Nunney <bigtallbill@gmail.com>
2018-03-11 15:02:55 -0400

    Add ability to create categories

--- handlers/category.go
+++ handlers/category.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"encoding/json"
 
+	"fmt"
 	. "github.com/liverecord/server/common/frame"
 	"github.com/liverecord/server/model"
 )
@@ -15,5 +16,31 @@ func (Ctx *AppContext) CategoryList(frame Frame) {
 		Ctx.Ws.WriteJSON(Frame{Type: CategoryListFrame, Data: string(cats)})
 	} else {
 		Ctx.Logger.WithError(err)
+	}
+}
+
+func (Ctx *AppContext) CategorySave(frame Frame) {
+	if Ctx.IsAuthorized() {
+		var category model.Category
+		err := frame.BindJSON(&category)
+		Ctx.Logger.Info("Decoded category", category)
+		Ctx.Logger.Info("User", Ctx.User)
+		if err == nil {
+			if category.ID > 0 {
+				Ctx.Logger.WithField("msg", "Category updates not supported yet").Info()
+			} else {
+				category.ID = 0
+				fmt.Println(frame.Data)
+				err = Ctx.Db.Set("gorm:association_autoupdate", false).Save(&category).Error
+				Ctx.Ws.WriteJSON(Frame{Type: CategorySaveFrame, Data: category.ToJSON()})
+			}
+			if err != nil {
+				Ctx.Logger.WithError(err).Error("Unable to save category")
+			}
+		} else {
+			Ctx.Logger.WithError(err).Error("can't unmarshall category")
+		}
+	} else {
+		Ctx.Logger.WithField("msg", "Unauthorized category save call").Info()
 	}
 }
