534f3c586ad734678f3468772f0bf2d6d12e67ae
Philipp Tkachev <zoonman@gmail.com>
2018-05-19 23:39:03 -0400

    File upload, package renaming. Modified connection_pool.go

--- connection_pool.go
+++ connection_pool.go
@@ -4,16 +4,23 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// SocketStateMap type
 type SocketStateMap map[*websocket.Conn]bool
+
+// SocketConnectionsMap type
 type SocketConnectionsMap map[*websocket.Conn]*User
+
+// UserConnectionsMap type
 type UserConnectionsMap map[uint]SocketStateMap
 
+// ConnectionPool is intended to keep track of all connections
 type ConnectionPool struct {
 	Sockets SocketConnectionsMap
 	Users   UserConnectionsMap
 	outbox  map[*websocket.Conn]chan Frame
 }
 
+// NewConnectionPool is factory to create new pool of connections
 func NewConnectionPool() *ConnectionPool {
 	var pool ConnectionPool
 	pool.Sockets = make(SocketConnectionsMap)
@@ -22,12 +29,14 @@ func NewConnectionPool() *ConnectionPool {
 	return &pool
 }
 
+// AddConnection adds connection to the pool
 func (pool *ConnectionPool) AddConnection(conn *websocket.Conn) {
 	pool.Sockets[conn] = nil
 	pool.outbox[conn] = make(chan Frame)
 	go pool.dispatch(conn)
 }
 
+// Authenticate binds user identifier to a connection
 func (pool *ConnectionPool) Authenticate(conn *websocket.Conn, user *User) {
 	pool.Sockets[conn] = user
 	if _, ok := pool.Users[user.ID]; !ok {
@@ -36,6 +45,7 @@ func (pool *ConnectionPool) Authenticate(conn *websocket.Conn, user *User) {
 	pool.Users[user.ID][conn] = true
 }
 
+// Logout disaccosiates users and connections
 func (pool *ConnectionPool) Logout(user *User) {
 	for conn := range pool.Users[user.ID] {
 		pool.Sockets[conn] = nil
@@ -43,6 +53,7 @@ func (pool *ConnectionPool) Logout(user *User) {
 	}
 }
 
+// DropConnection closes connection
 func (pool *ConnectionPool) DropConnection(conn *websocket.Conn) {
 	if pool.Sockets[conn] != nil {
 		delete(pool.Users[pool.Sockets[conn].ID], conn)
@@ -55,18 +66,21 @@ func (pool *ConnectionPool) DropConnection(conn *websocket.Conn) {
 	conn.Close()
 }
 
+// Broadcast sends a frame to all connections
 func (pool *ConnectionPool) Broadcast(frame Frame) {
 	for conn := range pool.Sockets {
 		pool.Write(conn, frame)
 	}
 }
 
+// Write sends a frame to a specific connection
 func (pool *ConnectionPool) Write(conn *websocket.Conn, frame Frame) {
 	if _, ok := pool.outbox[conn]; ok {
 		pool.outbox[conn] <- frame
 	}
 }
 
+// Send delivers a frame to all user's connections
 func (pool *ConnectionPool) Send(to *User, frame Frame) {
 	if _, ok := pool.Users[to.ID]; ok {
 		for conn := range pool.Users[to.ID] {
@@ -75,11 +89,9 @@ func (pool *ConnectionPool) Send(to *User, frame Frame) {
 	}
 }
 
+// actually sends frames
 func (pool *ConnectionPool) dispatch(c *websocket.Conn) {
 	for f := range pool.outbox[c] {
-		// f, _ := <-pool.outbox[c]
-		// time.Sleep(100 * time.Millisecond) // testing network delays
-
 		err := c.WriteJSON(&f)
 		if err != nil {
 			pool.DropConnection(c)
