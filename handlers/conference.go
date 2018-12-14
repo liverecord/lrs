89c000aa551e0ba48555208af131dcc169a10d97
Philipp Tkachev <zoonman@gmail.com>
2018-12-14 18:08:31 -0500

    SSR, Web-conf research

--- handlers/conference.go
+++ handlers/conference.go
@@ -9,6 +9,10 @@ func (Ctx *ConnCtx) CallInit(frame lrs.Frame) {
 	Ctx.Pool.Broadcast(frame, Ctx.Ws)
 }
 
+func (Ctx *ConnCtx) CallStop(frame lrs.Frame) {
+	Ctx.Pool.Broadcast(frame, Ctx.Ws)
+}
+
 // CallCandidate sends ICECandidate data
 func (Ctx *ConnCtx) CallCandidate(frame lrs.Frame) {
 	Ctx.Pool.Broadcast(frame, Ctx.Ws)
